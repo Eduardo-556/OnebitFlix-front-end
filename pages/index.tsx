c9899281f2e9e7701ad2e1c65d427241b03a1fd0
Eduardo Silva <eduardo_silva800@hotmail.com>
2023-07-16 18:17:08 -0300

    Criação do header da página homeNoAuth

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,22 @@
 import Head from "next/head";
 import styles from "../styles/homeNoAuth.module.scss";
+import HeaderNoAuth from "@/src/components/homeNoAuth/headernoAuth";
 
 const HomeNotAuth = function () {
   return (
     <>
-      <Head>a</Head>
-      <main></main>
+      <Head>
+        <title>Onebitflix</title>
+        <link rel="shortcut icon" href="/favicon.svg" type="image/x-icon" />
+        <meta property="og:title" content="Onebitflix" key="title" />
+        <meta
+          name="description"
+          content="Tenha acesso aos melhores conteúdos sobre programação de uma forma simples e fácil."
+        />
+      </Head>
+      <main>
+        <HeaderNoAuth />
+      </main>
     </>
   );
 };
