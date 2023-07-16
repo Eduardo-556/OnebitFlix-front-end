7b53909fd068d94aa1917e353c66815aceae3620
Eduardo Silva <eduardo_silva800@hotmail.com>
2023-07-16 05:13:59 -0300

    Mudanças nas pastas do projeto

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from "next/head";
+import styles from "../styles/homeNoAuth.module.scss";
+
+const HomeNotAuth = function () {
+  return (
+    <>
+      <Head>a</Head>
+      <main></main>
+    </>
+  );
+};
+
+export default HomeNotAuth;
