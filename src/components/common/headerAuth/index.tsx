2c99ef1d6ecbac98f256a621ae80fc07f98503fa
Eduardo Silva <eduardo_silva800@hotmail.com>
2023-07-27 17:37:02 -0300

    Crição e estilização dos elementos do header logado

--- src/components/common/headerAuth/index.tsx
+++ src/components/common/headerAuth/index.tsx
@@ -1,8 +1,28 @@
 import { Container, Form, Input } from "reactstrap";
 import styles from "./styles.module.scss";
 import Link from "next/link";
+import { useState } from "react";
+import Modal from "react-modal";
+import { symlink } from "fs";
+import { useRouter } from "next/router";
+
+Modal.setAppElement("#__next");
 
 const HeaderAuth = function () {
+  const router = useRouter();
+  const handleLogout = () => {
+    sessionStorage.clear();
+    router.push("/");
+  };
+  const [modalOpen, setModalOpen] = useState(false);
+
+  const handleOpenModal = () => {
+    setModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setModalOpen(false);
+  };
   return (
     <>
       <Container className={styles.nav}>
@@ -27,8 +47,24 @@ const HeaderAuth = function () {
             alt="lupaHeader"
             className={styles.searchImg}
           />
-          <p className={styles.userProfile}>AB</p>
+          <p className={styles.userProfile} onClick={handleOpenModal}>
+            AB
+          </p>
         </div>
+        <Modal
+          isOpen={modalOpen}
+          onRequestClose={handleCloseModal}
+          shouldCloseOnEsc={true}
+          className={styles.modal}
+          overlayClassName={styles.overlayModal}
+        >
+          <Link href="/profile" className="text-decoration-none">
+            <p className={styles.modalLink}>Meus Dados</p>
+          </Link>
+          <p className={styles.modalLink} onClick={handleLogout}>
+            Sair
+          </p>
+        </Modal>
       </Container>
     </>
   );
