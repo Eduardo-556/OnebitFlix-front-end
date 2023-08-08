d3c5a9279aa042eb2ce310c5b5f35741496fc0c2
Eduardo Silva <eduardo_silva800@hotmail.com>
2023-08-08 17:54:57 -0300

    Criação do metado search e comunicação com todo o front.

--- src/components/common/headerAuth/index.tsx
+++ src/components/common/headerAuth/index.tsx
@@ -1,7 +1,7 @@
 import { Container, Form, Input } from "reactstrap";
 import styles from "./styles.module.scss";
 import Link from "next/link";
-import { useEffect, useState } from "react";
+import { FormEvent, useEffect, useState } from "react";
 import Modal from "react-modal";
 import { symlink } from "fs";
 import { useRouter } from "next/router";
@@ -11,6 +11,21 @@ Modal.setAppElement("#__next");
 
 const HeaderAuth = function () {
   const [initials, setInitials] = useState("");
+
+  const handleSearch = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    router.push(`/search?name=${searchName}`);
+    setSearchName("");
+  };
+
+  const handleSearchClick = () => {
+    router.push(`/search?name=${searchName}`);
+    setSearchName("");
+  };
+
+  const [searchName, setSearchName] = useState("");
+
   useEffect(() => {
     profileService.fetchCurrent().then((user) => {
       const firstNameInitial = user.firstName.slice(0, 1);
@@ -32,6 +47,7 @@ const HeaderAuth = function () {
   const handleCloseModal = () => {
     setModalOpen(false);
   };
+
   return (
     <>
       <Container className={styles.nav}>
@@ -43,18 +59,23 @@ const HeaderAuth = function () {
           />
         </Link>
         <div className="d-flex align-items-center">
-          <Form>
+          <Form onSubmit={handleSearch}>
             <Input
               name="search"
               type="search"
               placeholder="Pesquisar"
               className={styles.input}
+              value={searchName}
+              onChange={(event) => {
+                setSearchName(event.currentTarget.value.toLowerCase());
+              }}
             />
           </Form>
           <img
             src="/homeAuth/iconSearch.svg"
             alt="lupaHeader"
             className={styles.searchImg}
+            onClick={handleSearchClick}
           />
           <p className={styles.userProfile} onClick={handleOpenModal}>
             {initials}
