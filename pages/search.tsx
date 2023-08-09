403c8557b01592a552fdee74925a986429a46129
Eduardo Silva <eduardo_silva800@hotmail.com>
2023-08-09 16:46:07 -0300

    Finalização da page search e do seu estilo visual

--- pages/search.tsx
+++ pages/search.tsx
@@ -3,6 +3,10 @@ import courseService, { CourseType } from "@/src/services/courseService";
 import Head from "next/head";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
+import styles from "../styles/search.module.scss";
+import { Container } from "reactstrap";
+import SearchCard from "@/src/components/searchCard";
+import Footer from "@/src/components/common/footer";
 
 const Search = function () {
   const router = useRouter();
@@ -21,15 +25,31 @@ const Search = function () {
     <>
       <Head>
         <title>Onebitflix - {"searchName"}</title>
-        <link rel="shortcut icon" href="/favicon.svg" type="image/x-icon" />
+        <link
+          rel="shortcut icon"
+          href="/favicon.svg"
+          type="image/x-icon"
+          className="text-decoration-none"
+        />
       </Head>
-      <main>
-        <HeaderAuth />
-        {searchResult?.map((course) => (
-          <div key={course.id}>
-            <p>{course.name}</p>
+      <main className={styles.main}>
+        <div className={styles.headerFooterBg}>
+          <HeaderAuth />
+        </div>
+        {searchResult.length >= 1 ? (
+          <div className={styles.searchResult}>
+            <Container className="d-flex flex-wrap justify-content-center gap-5 py-4">
+              {searchResult?.map((course) => (
+                <SearchCard key={course.id} course={course} />
+              ))}
+            </Container>
           </div>
-        ))}
+        ) : (
+          <p className={styles.noSearchText}>Nenhum resultado encontrado!</p>
+        )}
+        <div className={styles.headerFooterBg}>
+          <Footer />
+        </div>
       </main>
     </>
   );
